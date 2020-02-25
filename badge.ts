0e15972d8af475362d8217e43b9c09160330ffb5
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-25 23:13:39 +0100

    :sparkles: Add badge width logic

--- badge.ts
+++ badge.ts
@@ -1,5 +1,6 @@
 import { LIGHT, ORANGE, DARK } from "./badges";
 import { getNumberOfUpvotes } from "./hacker-news";
+import width from "string-pixel-width";
 
 export const getBadge = async (
   id: number,
@@ -12,9 +13,16 @@ export const getBadge = async (
   if (type === "orange") badge = ORANGE;
   if (type === "dark") badge = DARK;
   const upvotes = await getNumberOfUpvotes(id);
-  const textWidth = 3;
+  const textWidth = width(hackerNews, { size: 25 });
+  const numberWidth = width(upvotes.toString(), { size: 18 });
   return badge
-    .replace(/{WIDTH}/g, (139 + textWidth).toString())
+    .replace(/{WIDTH}/g, (110 + numberWidth + textWidth).toString())
+    .replace(/{SHORT_WIDTH}/g, (110 + numberWidth + textWidth - 2).toString())
+    .replace(/{NUM_POS}/g, (textWidth + 90).toString())
+    .replace(
+      /{TRIANGLE_POS}/g,
+      (textWidth + 90 + (numberWidth - 19) / 2).toString()
+    )
     .replace(/{FONT}/g, font)
     .replace(/{HACKER_NEWS}/g, hackerNews)
     .replace(/{UPVOTES}/g, upvotes.toString())
